344021a78a199d64524fae138aa0c00a9a419433
Khaled Zeffouni <83452303+khaled436@users.noreply.github.com>
2025-05-17 20:53:57 +0200

    154 creating a custom directive

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -2,10 +2,22 @@ import {Directive} from "@angular/core";
 
 @Directive({
   selector: 'a[appSafeLink]',
-  standalone: true
+  standalone: true,
+  host: {
+    '(click)':'onLeavingSite($event)'
+  }
 })
 export class SafeLinkDirective {
   constructor() {
     console.log('safe link active');
+  }
+
+  onLeavingSite(event: MouseEvent){
+    const   wantsToLeave = window.confirm('do you want to leave the site ?');
+    if(wantsToLeave){
+      return;
+    }
+
+    event.preventDefault();
   }
 }
