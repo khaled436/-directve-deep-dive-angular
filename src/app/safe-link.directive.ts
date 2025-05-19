ec1fe393f03d14812d267b27c8bde5f0441899d2
Khaled Zeffouni <83452303+khaled436@users.noreply.github.com>
2025-05-19 19:48:41 +0200

    155 inputs in custom directive

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import {Directive} from "@angular/core";
+import {Directive, input} from "@angular/core";
 
 @Directive({
   selector: 'a[appSafeLink]',
@@ -8,12 +8,17 @@ import {Directive} from "@angular/core";
   }
 })
 export class SafeLinkDirective {
+
+  queryParam = input('myapp',{alias: 'appSafeLink'});
+
   constructor() {
     console.log('safe link active');
   }
 
   onLeavingSite(event: MouseEvent){
     const   wantsToLeave = window.confirm('do you want to leave the site ?');
+    const link = (event.target as HTMLAnchorElement).href;
+    (event.target as HTMLAnchorElement).href = link + '?from=' + this.queryParam();
     if(wantsToLeave){
       return;
     }
